40833e725b8de2aa6be3df7f8138cc2f79010dac
Luke Skarth-Hayley <lukesk@outlook.com>
2021-08-05 15:10:28 +0100

    move shaderswitcher into plugin for demo and test

---
+++ Scripts/Utilities/ShaderSwitcher.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderSwitcher : MonoBehaviour
+{
+    Material defaultMaterial;
+    Material lastMaterial;
+    public List<Material> switchMaterial = new List<Material>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        defaultMaterial = GetComponent<Renderer>().material;
+    }
+
+    public void SetSwitchShader (int shaderRef)
+    {
+        lastMaterial = GetComponent<Renderer>().material;
+        GetComponent<Renderer>().material = switchMaterial[shaderRef];
+    }
+
+    public void UnsetSwitchShader ()
+    {
+        GetComponent<Renderer>().material = defaultMaterial;
+    }
+}
