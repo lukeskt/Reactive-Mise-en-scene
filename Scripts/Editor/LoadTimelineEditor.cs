b0999b34a08e9aeb53ac2a8120429905f31eacdb
Luke Skarth-Hayley <hello@lukeskarth.com>
2021-09-01 22:41:34 +0100

    Load timeline and custom editor first draft

--- Scripts/Editor/LoadTimelineEditor.cs
+++ Scripts/Editor/LoadTimelineEditor.cs
@@ -6,20 +6,79 @@ using UnityEngine;
 
 namespace ReactiveMiseEnScene
 {
+    [CustomEditor(typeof(LoadTimeline))]
+    [CanEditMultipleObjects]
     public class LoadTimelineEditor : Editor
     {
-        SerializedProperty locale;
-        SerializedProperty tendency;
-        SerializedProperty timeline;
+        SerializedProperty RMSettings;
+        SerializedProperty algorithm;
+        SerializedProperty requestType;
+
+        SerializedProperty presetTendency;
+        string[] editorTendency;
+        SerializedProperty tendencyIndex;
+        
+        SerializedProperty localeRequest;
+        string[] editorLocale;        
+        SerializedProperty localeIndex;
+
+        SerializedProperty timelines;
+        SerializedProperty presetTimeline;
 
         private void OnEnable()
         {
-            
+            RMSettings = serializedObject.FindProperty("RMSettings");
+            algorithm = serializedObject.FindProperty("algorithm");
+            var loadTimeline = target as LoadTimeline;
+            if (loadTimeline.RMSettings != null)
+            {
+                editorTendency = loadTimeline.RMSettings.Tendencies;
+                editorLocale = loadTimeline.RMSettings.Locales;
+            }
+            requestType = serializedObject.FindProperty("requestType");
+            presetTendency = serializedObject.FindProperty("presetTendency");
+            tendencyIndex = serializedObject.FindProperty("tendencyIndex");
+            localeRequest = serializedObject.FindProperty("localeRequest");
+            localeIndex = serializedObject.FindProperty("localeIndex");
+            timelines = serializedObject.FindProperty("timelines");
+            presetTimeline = serializedObject.FindProperty("presetTimeline");
         }
 
         public override void OnInspectorGUI()
         {
-            base.OnInspectorGUI();
+            serializedObject.Update();
+            EditorGUILayout.PropertyField(RMSettings);
+            serializedObject.ApplyModifiedProperties();
+            serializedObject.Update();
+            EditorGUILayout.PropertyField(algorithm);
+            var loadTimeline = target as LoadTimeline;
+            if (loadTimeline.RMSettings != null)
+            {
+                editorTendency = loadTimeline.RMSettings.Tendencies;
+                if (loadTimeline.algorithm == ReactiveMesSettings.TendencyAlgorithm.Preset) // preset algo - index not ideal, name match how?
+                {
+                    tendencyIndex.intValue = EditorGUILayout.Popup("Preset Tendency:", tendencyIndex.intValue, editorTendency);
+                }
+                presetTendency.stringValue = editorTendency[tendencyIndex.intValue];
+
+                EditorGUILayout.PropertyField(requestType);
+
+                editorLocale = loadTimeline.RMSettings.Locales;
+                if (loadTimeline.requestType == ReactiveMesSettings.RequestType.Locale)
+                {
+                    localeIndex.intValue = EditorGUILayout.Popup("Locale to Request:", localeIndex.intValue, editorLocale);
+                }
+                localeRequest.stringValue = editorLocale[localeIndex.intValue];
+            }
+            if (loadTimeline.algorithm == ReactiveMesSettings.TendencyAlgorithm.Preset)
+            {
+                EditorGUILayout.PropertyField(presetTimeline);
+            }
+            else
+            {
+                EditorGUILayout.PropertyField(timelines);
+            }
+            serializedObject.ApplyModifiedProperties();
         }
     }
 }
--- Scripts/Loaders/LoadTimeline.cs
+++ Scripts/Loaders/LoadTimeline.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.Timeline;
 
 namespace ReactiveMiseEnScene
 {
@@ -9,13 +11,17 @@ namespace ReactiveMiseEnScene
     public class LoadTimeline : MonoBehaviour
     {
         public ReactiveMesSettings RMSettings;
+        public ReactiveMesSettings.TendencyAlgorithm algorithm;
+        public ReactiveMesSettings.RequestType requestType;
+        public string presetTendency;
         [HideInInspector] public int localeIndex = 0; // for custom editor
+        public string localeRequest;
         [HideInInspector] public int tendencyIndex = 0; // for custom editor
 
-        public ReactiveMesSettings.TendencyAlgorithm tendencyAlgorithm = ReactiveMesSettings.TendencyAlgorithm.MaxValue;
-
-        Playable timeline;
-        PlayableDirector timelineDirector;
+        private PlayableDirector timelineDirector;
+        public List<TimelineAsset> timelines;
+        public TimelineAsset presetTimeline;
+        private ReactiveMesDataManager DataMgr;
 
         // times if we need them
         private float startTime;
@@ -24,13 +30,57 @@ namespace ReactiveMiseEnScene
         // Start is called before the first frame update
         void Start()
         {
+            timelineDirector = GetComponent<PlayableDirector>();
+            // Make sure timeline stopped before loading new timeline.
+            timelineDirector.Stop();
+            DataMgr = FindObjectOfType<ReactiveMesDataManager>();
+            
             startTime = Time.time;
-        }
 
-        // Update is called once per frame
-        void Update()
-        {
+            Dictionary<string, double> TendenciesFromDataMgr = new Dictionary<string, double>();
+            string TendencyForTimeline;
+            switch (requestType)
+            {
+                case ReactiveMesSettings.RequestType.Global:
+                    TendenciesFromDataMgr = DataMgr.GetGlobalTendency(DataMgr.attentionObjects);
+                    break;
+                case ReactiveMesSettings.RequestType.Locale:
+                    TendenciesFromDataMgr = DataMgr.GetLocaleTendency(DataMgr.attentionObjects, localeRequest);
+                    break;
+                default:
+                    break;
+            }
 
+            switch (algorithm)
+            {
+                case ReactiveMesSettings.TendencyAlgorithm.MaxValue:
+                    TendencyForTimeline = TendenciesFromDataMgr.Aggregate((l, r) => l.Value > r.Value ? l : r).Key;
+                    timelineDirector.playableAsset = timelines.Find(profile => profile.name.Contains(TendencyForTimeline.ToString()));
+                    break;
+                case ReactiveMesSettings.TendencyAlgorithm.MinValue:
+                    TendencyForTimeline = TendenciesFromDataMgr.Aggregate((l, r) => l.Value < r.Value ? l : r).Key;
+                    timelineDirector.playableAsset = timelines.Find(profile => profile.name.Contains(TendencyForTimeline.ToString()));
+                    break;
+                case ReactiveMesSettings.TendencyAlgorithm.Proportional:
+                    NotImpl();
+                    break;
+                case ReactiveMesSettings.TendencyAlgorithm.InverseProportion:
+                    NotImpl();
+                    break;
+                case ReactiveMesSettings.TendencyAlgorithm.CompetitorDistribution:
+                    NotImpl();
+                    break;
+                case ReactiveMesSettings.TendencyAlgorithm.Preset:
+                    timelineDirector.playableAsset = presetTimeline;
+                    break;
+                case ReactiveMesSettings.TendencyAlgorithm.Random:
+                    timelineDirector.playableAsset = timelines[Random.Range(0, timelines.Count)];
+                    break;
+                default:
+                    goto case ReactiveMesSettings.TendencyAlgorithm.Preset;
+            }
+            // Start timeline after selecting one based on algorithm.
+            timelineDirector.Play();
         }
 
         private bool CheckTimeSinceStart(float timeToWait)
@@ -44,6 +94,10 @@ namespace ReactiveMiseEnScene
             {
                 return false;
             }
+        }
+        private void NotImpl()
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
