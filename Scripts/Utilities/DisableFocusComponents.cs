35b3162133b8a8a03608c6e105a2479d1afb4ca8
Luke Skarth-Hayley <hello@lukeskarth.com>
2021-09-08 19:13:06 +0100

    add script to remove focus and focus measures from specified objects in array

---
+++ Scripts/Utilities/DisableFocusComponents.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ReactiveMiseEnScene
+{
+    public class DisableFocusComponents : MonoBehaviour
+    {
+        public GameObject[] objectsToRemoveComponentFrom;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            foreach (var obj in objectsToRemoveComponentFrom)
+            {
+                Destroy(obj.GetComponent<FocusMeasures>());
+                Destroy(obj.GetComponent<Focus>());
+            }
+        }
+    }
+}
