0fd982a553bb5d40346ecd9650e9b8f4349d8ff1
Luke Skarth-Hayley <hello@lukeskarth.com>
2021-09-08 10:52:42 +0100

    add print debug function for testing focus events

---
+++ Scripts/Utilities/FocusDebugPrint.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ReactiveMiseEnScene
+{
+    public class FocusDebugPrint : MonoBehaviour
+    {
+        public void PrintThis (string text)
+        {
+            Debug.Log(text);
+        }
+    }
+}
