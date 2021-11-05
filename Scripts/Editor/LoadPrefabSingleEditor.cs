d94149c0cf52b72c04baac2155280b82a467c6e1
Luke Skarth-Hayley <hello@lukeskarth.com>
2021-11-05 22:46:19 +0000

    add single prefab loader, might be easier to use
    rather than monolithic multiprefab loading from tendencies, simplifies algorithms too for future refactoring

---
+++ Scripts/Editor/LoadPrefabSingleEditor.cs
@@ -0,0 +1,72 @@
+#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace ReactiveMiseEnScene
+{
+    [CustomEditor(typeof(LoadPrefabSingle))]
+    [CanEditMultipleObjects]
+    public class LoadPrefabSingleEditor : Editor
+    {
+        SerializedProperty RMSettings;
+        SerializedProperty algorithm;
+        SerializedProperty requestType;
+
+        SerializedProperty localeRequest;
+        string[] editorLocale;
+        SerializedProperty localeIndex;
+
+        SerializedProperty continuous;
+
+        SerializedProperty tendencyObjects;
+
+        private void OnEnable()
+        {
+            RMSettings = serializedObject.FindProperty("RMSettings");
+            algorithm = serializedObject.FindProperty("algorithm");
+            requestType = serializedObject.FindProperty("requestType");
+            var loadPrefabSingle = target as LoadPrefabSingle;
+            if (loadPrefabSingle.RMSettings != null)
+            {
+                editorLocale = loadPrefabSingle.RMSettings.Locales;
+            }
+            localeRequest = serializedObject.FindProperty("localeRequest");
+            localeIndex = serializedObject.FindProperty("localeIndex");
+            continuous = serializedObject.FindProperty("continuous");
+            tendencyObjects = serializedObject.FindProperty("tendencyObjects");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            EditorGUILayout.PropertyField(RMSettings);
+            serializedObject.ApplyModifiedProperties();
+            serializedObject.Update();
+
+            EditorGUILayout.PropertyField(algorithm);
+
+            var loadPrefabSingle = target as LoadPrefabSingle;
+
+            if (loadPrefabSingle.RMSettings != null)
+            {
+                if (loadPrefabSingle.algorithm != ReactiveMesSettings.SingleResultTendencyAlgorithm.Random)
+                {
+                    EditorGUILayout.PropertyField(requestType);
+                    editorLocale = loadPrefabSingle.RMSettings.Locales;
+                    if (loadPrefabSingle.requestType == ReactiveMesSettings.RequestType.Locale)
+                    {
+                        localeIndex.intValue = EditorGUILayout.Popup("Locale to Request:", localeIndex.intValue, editorLocale);
+                    }
+                    localeRequest.stringValue = editorLocale[localeIndex.intValue];
+                }
+            }
+            //EditorGUILayout.PropertyField(continuous); // uncomment to show continuous check.
+            EditorGUILayout.Space();
+            EditorGUILayout.PropertyField(tendencyObjects);
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
+#endif
---
+++ Scripts/Loaders/LoadPrefabSingle.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace ReactiveMiseEnScene
+{
+    public class LoadPrefabSingle : MonoBehaviour
+    {
+        public ReactiveMesSettings RMSettings;
+        ReactiveMesDataManager DataMgr;
+
+        public ReactiveMesSettings.SingleResultTendencyAlgorithm algorithm;
+        public ReactiveMesSettings.RequestType requestType;
+
+        [HideInInspector] public int tendencyIndex = 0; // for custom editor
+
+        public string localeRequest;
+        [HideInInspector] public int localeIndex = 0; // for custom editor
+
+        [Tooltip("WARNING EXPERIMENTAL FEATURE!\nIf Continuous is checked, update the tendency-based timeline every frame.")]
+        public bool continuous = false;
+
+        public List<GameObject> tendencyObjects;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            DataMgr = FindObjectOfType<ReactiveMesDataManager>();
+            PrefabLoader(localeRequest);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (continuous)
+            {
+                PrefabLoader(localeRequest); // change this to the locale locale?
+            }
+        }
+
+        private void PrefabLoader(string localeRequest)
+        {
+            Dictionary<string, double> TendenciesFromDataMgr = new Dictionary<string, double>();
+            string TendencyForPrefab;
+            switch (requestType)
+            {
+                case ReactiveMesSettings.RequestType.Global:
+                    TendenciesFromDataMgr = DataMgr.GetGlobalTendency(DataMgr.reactiveObjects);
+                    break;
+                case ReactiveMesSettings.RequestType.Locale:
+                    TendenciesFromDataMgr = DataMgr.GetLocaleTendency(DataMgr.reactiveObjects, localeRequest);
+                    break;
+                default:
+                    break;
+            }
+
+            switch (algorithm)
+            {
+                case ReactiveMesSettings.SingleResultTendencyAlgorithm.StrongestTendency:
+                    TendencyForPrefab = TendenciesFromDataMgr.Aggregate((l, r) => l.Value > r.Value ? l : r).Key;
+                    spawnObject(tendencyObjects.Find(profile => profile.name.Contains(TendencyForPrefab)), gameObject);
+                    break;
+                case ReactiveMesSettings.SingleResultTendencyAlgorithm.SecondStrongest:
+                    var SortedTendencies = TendenciesFromDataMgr.ToList().OrderBy(x => x.Value).Reverse().ToList();
+                    spawnObject(tendencyObjects.Find(profile => profile.name.Contains(SortedTendencies[1].Key)), gameObject);
+                    break;
+                case ReactiveMesSettings.SingleResultTendencyAlgorithm.SecondWeakest:
+                    var UnsortTendencies = TendenciesFromDataMgr.ToList().OrderBy(x => x.Value).ToList();
+                    spawnObject(tendencyObjects.Find(profile => profile.name.Contains(UnsortTendencies[1].Key)), gameObject);
+                    break;
+                case ReactiveMesSettings.SingleResultTendencyAlgorithm.WeakestTendency:
+                    TendencyForPrefab = TendenciesFromDataMgr.Aggregate((l, r) => l.Value < r.Value ? l : r).Key;
+                    spawnObject(tendencyObjects.Find(profile => profile.name.Contains(TendencyForPrefab)), gameObject);
+                    break;
+                case ReactiveMesSettings.SingleResultTendencyAlgorithm.Random:
+                    spawnObject(tendencyObjects[UnityEngine.Random.Range(0, tendencyObjects.Count)], gameObject);
+                    break;
+                default:
+                    goto case ReactiveMesSettings.SingleResultTendencyAlgorithm.StrongestTendency;
+            }
+
+            print("PING! WE DID IT!");
+        }
+
+        private void spawnObject(GameObject objectToSpawn, GameObject placementPoint)
+        {
+            Instantiate(objectToSpawn, placementPoint.transform.position, placementPoint.transform.rotation, placementPoint.transform);
+            //Destroy(placementPoint);
+        }
+    }
+}
