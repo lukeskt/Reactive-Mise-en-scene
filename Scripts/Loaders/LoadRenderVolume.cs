98350ea4f8046742d9e093c56a431a946493c46c
Luke Skarth-Hayley <hello@lukeskarth.com>
2021-11-05 22:47:15 +0000

    renames of attention to reactive objects for clarity in vol and timeline loaders, add and update algo names

--- Scripts/Loaders/LoadRenderVolume.cs
+++ Scripts/Loaders/LoadRenderVolume.cs
@@ -16,6 +16,9 @@ namespace ReactiveMiseEnScene
         public string localeRequest;
         [HideInInspector] public int localeIndex = 0; // for custom editor
 
+        [Tooltip("WARNING EXPERIMENTAL FEATURE!\nIf Continuous is checked, update the tendency-based volume profile every frame.")]
+        public bool continuous = false;
+
         private Volume volume;
         public List<VolumeProfile> volumeProfiles;
         public VolumeProfile presetVolumeProfile;
@@ -25,8 +28,20 @@ namespace ReactiveMiseEnScene
         void Start()
         {
             volume = GetComponent<Volume>();
-
             DataMgr = FindObjectOfType<ReactiveMesDataManager>();
+            RenderVolumeLoader(localeRequest);
+        }
+
+        private void Update()
+        {
+            if (continuous)
+            {
+                RenderVolumeLoader(localeRequest); // change this to the local locale?
+            }
+        }
+
+        private void RenderVolumeLoader (string localeToRequest)
+        {
             Dictionary<string, double> TendenciesFromDataMgr = new Dictionary<string, double>();
             string TendencyForVolProfile;
             switch (requestType)
@@ -34,10 +49,10 @@ namespace ReactiveMiseEnScene
                 // note here: these are max-value derived, so akin to first-past the post... maybe check a threshold?
                 // also offer an inversion / min value?
                 case ReactiveMesSettings.RequestType.Global:
-                    TendenciesFromDataMgr = DataMgr.GetGlobalTendency(DataMgr.attentionObjects);
+                    TendenciesFromDataMgr = DataMgr.GetGlobalTendency(DataMgr.reactiveObjects);
                     break;
                 case ReactiveMesSettings.RequestType.Locale:
-                    TendenciesFromDataMgr = DataMgr.GetLocaleTendency(DataMgr.attentionObjects, localeRequest);
+                    TendenciesFromDataMgr = DataMgr.GetLocaleTendency(DataMgr.reactiveObjects, localeToRequest);
                     break;
                 default:
                     break;
@@ -45,15 +60,19 @@ namespace ReactiveMiseEnScene
 
             switch (algorithm)
             {
-                case ReactiveMesSettings.SingleResultTendencyAlgorithm.MaxValue:
+                case ReactiveMesSettings.SingleResultTendencyAlgorithm.StrongestTendency:
                     TendencyForVolProfile = TendenciesFromDataMgr.Aggregate((l, r) => l.Value > r.Value ? l : r).Key;
                     volume.profile = volumeProfiles.Find(profile => profile.name.Contains(TendencyForVolProfile));
                     break;
-                case ReactiveMesSettings.SingleResultTendencyAlgorithm.RunnerUp:
+                case ReactiveMesSettings.SingleResultTendencyAlgorithm.SecondStrongest:
                     var SortedTendencies = TendenciesFromDataMgr.ToList().OrderBy(x => x.Value).Reverse().ToList();
                     volume.profile = volumeProfiles.Find(profile => profile.name.Contains(SortedTendencies[1].Key));
                     break;
-                case ReactiveMesSettings.SingleResultTendencyAlgorithm.MinValue:
+                case ReactiveMesSettings.SingleResultTendencyAlgorithm.SecondWeakest:
+                    var UnsortTendencies = TendenciesFromDataMgr.ToList().OrderBy(x => x.Value).ToList();
+                    volume.profile = volumeProfiles.Find(profile => profile.name.Contains(UnsortTendencies[1].Key));
+                    break;
+                case ReactiveMesSettings.SingleResultTendencyAlgorithm.WeakestTendency:
                     TendencyForVolProfile = TendenciesFromDataMgr.Aggregate((l, r) => l.Value < r.Value ? l : r).Key;
                     volume.profile = volumeProfiles.Find(profile => profile.name.Contains(TendencyForVolProfile));
                     break;
@@ -64,7 +83,7 @@ namespace ReactiveMiseEnScene
                     volume.profile = volumeProfiles[Random.Range(0, volumeProfiles.Count)];
                     break;
                 default:
-                    goto case ReactiveMesSettings.SingleResultTendencyAlgorithm.MaxValue;
+                    goto case ReactiveMesSettings.SingleResultTendencyAlgorithm.StrongestTendency;
             }
         }
     }
--- Scripts/Loaders/LoadTimeline.cs
+++ Scripts/Loaders/LoadTimeline.cs
@@ -17,6 +17,9 @@ namespace ReactiveMiseEnScene
         public string localeRequest;
         [HideInInspector] public int localeIndex = 0; // for custom editor
 
+        [Tooltip("WARNING EXPERIMENTAL FEATURE!\nIf Continuous is checked, update the tendency-based timeline every frame.")]
+        public bool continuous = false;
+
         private PlayableDirector timelineDirector;
         public List<TimelineAsset> timelines;
         public TimelineAsset presetTimeline;
@@ -28,49 +31,63 @@ namespace ReactiveMiseEnScene
         void Start()
         {
             timelineDirector = GetComponent<PlayableDirector>();
+            DataMgr = FindObjectOfType<ReactiveMesDataManager>();
+            TimelineLoader(localeRequest);
+        }
+
+        private void Update()
+        {
+            if (continuous)
+            {
+                TimelineLoader(localeRequest); // change this to the local locale?
+            }
+        }
+
+        private void TimelineLoader (string localeToRequest)
+        {
+
             // Make sure timeline stopped before loading new timeline.
             timelineDirector.Stop();
-            DataMgr = FindObjectOfType<ReactiveMesDataManager>();
 
             Dictionary<string, double> TendenciesFromDataMgr = new Dictionary<string, double>();
             string TendencyForTimeline;
             switch (requestType)
             {
                 case ReactiveMesSettings.RequestType.Global:
-                    TendenciesFromDataMgr = DataMgr.GetGlobalTendency(DataMgr.attentionObjects);
+                    TendenciesFromDataMgr = DataMgr.GetGlobalTendency(DataMgr.reactiveObjects);
                     break;
                 case ReactiveMesSettings.RequestType.Locale:
-                    TendenciesFromDataMgr = DataMgr.GetLocaleTendency(DataMgr.attentionObjects, localeRequest);
+                    TendenciesFromDataMgr = DataMgr.GetLocaleTendency(DataMgr.reactiveObjects, localeToRequest);
                     break;
                 default:
+
                     break;
             }
 
             switch (algorithm)
             {
-                case ReactiveMesSettings.SingleResultTendencyAlgorithm.MaxValue:
+                case ReactiveMesSettings.SingleResultTendencyAlgorithm.StrongestTendency:
                     TendencyForTimeline = TendenciesFromDataMgr.Aggregate((l, r) => l.Value > r.Value ? l : r).Key;
                     timelineDirector.playableAsset = timelines.Find(profile => profile.name.Contains(TendencyForTimeline));
                     break;
-                case ReactiveMesSettings.SingleResultTendencyAlgorithm.RunnerUp:
+                case ReactiveMesSettings.SingleResultTendencyAlgorithm.SecondStrongest:
                     var SortedTendencies = TendenciesFromDataMgr.ToList().OrderBy(x => x.Value).Reverse().ToList();
                     timelineDirector.playableAsset = timelines.Find(profile => profile.name.Contains(SortedTendencies[1].Key));
                     break;
-                case ReactiveMesSettings.SingleResultTendencyAlgorithm.MinValue:
+                case ReactiveMesSettings.SingleResultTendencyAlgorithm.SecondWeakest:
+                    var UnsortedTendencies = TendenciesFromDataMgr.ToList().OrderBy(x => x.Value).ToList();
+                    timelineDirector.playableAsset = timelines.Find(profile => profile.name.Contains(UnsortedTendencies[1].Key));
+                    break;
+                case ReactiveMesSettings.SingleResultTendencyAlgorithm.WeakestTendency:
                     TendencyForTimeline = TendenciesFromDataMgr.Aggregate((l, r) => l.Value < r.Value ? l : r).Key;
                     timelineDirector.playableAsset = timelines.Find(profile => profile.name.Contains(TendencyForTimeline));
                     break;
-                //case ReactiveMesSettings.SingleResultTendencyAlgorithm.Preset:
-                //    timelineDirector.playableAsset = presetTimeline;
-                //    break;
                 case ReactiveMesSettings.SingleResultTendencyAlgorithm.Random:
                     timelineDirector.playableAsset = timelines[Random.Range(0, timelines.Count)];
                     break;
                 default:
-                    goto case ReactiveMesSettings.SingleResultTendencyAlgorithm.MaxValue;
+                    goto case ReactiveMesSettings.SingleResultTendencyAlgorithm.StrongestTendency;
             }
-            // Start timeline after selecting one based on algorithm.
-            timelineDirector.Play();
         }
     }
 }
