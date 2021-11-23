9abe0829ab48886f3efdccab4a62b4e19f690a66
Luke Skarth-Hayley <hello@lukeskarth.com>
2021-11-23 17:19:22 +0000

    temp write of shader switcher to test new focus system

--- Scripts/Utilities/ShaderSwitcher.cs
+++ Scripts/Utilities/ShaderSwitcher.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ShaderSwitcher : MonoBehaviour
 {
+    Renderer objRenderer;
     Material defaultMaterial;
     Material lastMaterial;
     public List<Material> switchMaterial = new List<Material>();
@@ -11,13 +12,35 @@ public class ShaderSwitcher : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        objRenderer = GetComponent<Renderer>();
         defaultMaterial = GetComponent<Renderer>().material;
     }
 
     public void SetSwitchShader (int shaderRef)
     {
-        lastMaterial = GetComponent<Renderer>().material;
-        GetComponent<Renderer>().material = switchMaterial[shaderRef];
+        lastMaterial = objRenderer.material;
+        objRenderer.material = switchMaterial[shaderRef];
+    }
+
+    public void Focus2Reader(GameObject obj, ReactiveMiseEnScene.FocusLevel currentFocusLevel, ReactiveMiseEnScene.FocusLevel lastFocusLevel)
+    {
+        switch (currentFocusLevel)
+        {
+            case ReactiveMiseEnScene.FocusLevel.offscreen:
+                SetSwitchShader(0);
+                break;
+            case ReactiveMiseEnScene.FocusLevel.onscreen:
+                SetSwitchShader(1);
+                break;
+            case ReactiveMiseEnScene.FocusLevel.attended:
+                SetSwitchShader(2);
+                break;
+            case ReactiveMiseEnScene.FocusLevel.focused:
+                SetSwitchShader(3);
+                break;
+            default:
+                break;
+        }
     }
 
     public void UnsetSwitchShader ()
