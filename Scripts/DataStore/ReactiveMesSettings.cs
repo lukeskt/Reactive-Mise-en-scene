d9bc982bde0496b8874ae8d51d0737ce23f38e11
Luke Skarth-Hayley <hello@lukeskarth.com>
2021-12-22 13:49:27 +0000

    remove some old commented out code in settings

--- Scripts/DataStore/ReactiveMesSettings.cs
+++ Scripts/DataStore/ReactiveMesSettings.cs
@@ -7,7 +7,6 @@ namespace ReactiveMiseEnScene
     [CreateAssetMenu(fileName = "Data", menuName = "ReactiveMesSettings", order = 1)]
     public class ReactiveMesSettings : ScriptableObject
     {
-        // Need to figure out how to replace enums above with lists, then dropdown those in custom inspector.
         public string[] Locales = new string[4];
         public string[] Tendencies = new string[4];
 
@@ -16,21 +15,6 @@ namespace ReactiveMiseEnScene
             Global,
             Locale
         }
-
-        //public enum MultiResultTendencyAlgorithm
-        //{
-        //    MaxValue, // winning tendency by largest attention value
-        //    RunnerUp, // second place tendency
-        //    MinValue,
-        //    // FirstPastThreshold, // winning tendency past a threshold
-        //    Proportional, // proportion of decision/placement points distroed amongst tendencies
-        //    // ProportionalPastThreshold, // any that pass a threshold get a proportional rep
-        //    InverseProportion, // i.e. load inverse to what proportional results would be
-        //    // CompetitorDistribution, // distribute the tendency total amongst all others than the winning tendency
-        //    Preset,
-        //    Random
-        //}
-
         public enum SingleResultTendencyAlgorithm
         {
             StrongestTendency,
@@ -45,10 +29,9 @@ namespace ReactiveMiseEnScene
         private void OnEnable() { }
         private void OnValidate() { }
 
-    //    // Define other methods below?
         public void TestFunc()
         {
-            Debug.Log("testing reactivemediasettings SO!");
+            Debug.Log("Reactive Mise-en-scene Settings SO Test");
         }
     }
 }
