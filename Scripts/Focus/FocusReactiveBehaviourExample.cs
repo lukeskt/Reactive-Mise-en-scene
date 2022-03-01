3b5527e4ae239a81238f5fde0b5789a13773aa11
Luke Skarth-Hayley <hello@lukeskarth.com>
2022-03-01 10:28:33 +0000

    focus reactive behaviour example prints obj name
    make clearer what object is reactive to focus and what state

--- Scripts/Focus/FocusReactiveBehaviourExample.cs
+++ Scripts/Focus/FocusReactiveBehaviourExample.cs
@@ -6,10 +6,10 @@ namespace ReactiveMiseEnScene
 {
     public class FocusReactiveBehaviourExample : FocusReactiveBehaviour
     {
-        public override void Offscreen() { print("Offscreen!"); }
-        public override void Onscreen() { print("Onscreen!"); }
-        public override void Attended() { print("Attended!"); }
-        public override void Focused() { print("Focused!"); }
+        public override void Offscreen() { print($"{gameObject.name} is offscreen!"); }
+        public override void Onscreen() { print($"{gameObject.name} is onscreen!"); }
+        public override void Attended() { print($"{gameObject.name} is attended!"); }
+        public override void Focused() { print($"{gameObject.name} is focused!"); }
 
         public override void StayOffscreen() { }
         public override void StayOnscreen() { }
